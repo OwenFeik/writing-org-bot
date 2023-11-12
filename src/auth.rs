1fb9ec78f7fc4dffb1462a143d6a286e7f57363d
OwenFeik <owenhfeik@gmail.com>
2023-11-12 12:33:12 +1100

    implement signature verification

--- src/auth.rs
+++ src/auth.rs
@@ -1,3 +1,35 @@
-pub fn validate(signature: String) -> bool {
-    true
+use ed25519_dalek::{Signature, Verifier, VerifyingKey};
+
+use super::{err, Result};
+
+fn decode_hex_signature(hex: &str) -> Result<[u8; 64]> {
+    const SIGNATURE_LENGTH: usize = 64;
+    let mut decoded: [u8; SIGNATURE_LENGTH] = [0; SIGNATURE_LENGTH];
+    let mut len = 0;
+    for i in (0..hex.len()).step_by(2) {
+        decoded[i] = u8::from_str_radix(&hex[i..i + 2], 16).map_err(|e| e.to_string())?;
+        len += 1;
+    }
+
+    if len == SIGNATURE_LENGTH {
+        Ok(decoded)
+    } else {
+        err(format!("Invalid signature length: {} bytes.", len))
+    }
+}
+
+pub fn validate(sighex: &str, timestamp: &str, body: &str) -> Result<bool> {
+    const PUBLIC_KEY_LENGTH: usize = 32;
+    const PUBLIC_KEY: &[u8; PUBLIC_KEY_LENGTH] = &[
+        0xDC, 0x72, 0xF9, 0xAB, 0x92, 0x57, 0x1D, 0x06, 0x98, 0x7A, 0xF6, 0x5C, 0x54, 0x2B, 0x7B,
+        0x41, 0xF5, 0x6E, 0x01, 0x38, 0xEC, 0x9C, 0x01, 0x6B, 0xF9, 0x52, 0xD3, 0xC2, 0x62, 0x5F,
+        0xB3, 0x00,
+    ];
+
+    let verkey = VerifyingKey::from_bytes(PUBLIC_KEY).map_err(|e| format!("{e}"))?;
+    let decoded = decode_hex_signature(sighex)?;
+    let signature = Signature::from_bytes(&decoded);
+    let message = format!("{timestamp}{body}");
+
+    Ok(verkey.verify(message.as_bytes(), &signature).is_ok())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,72 @@
-use actix_web::{dev::Service, post, web, Responder};
+use actix_web::{
+    error::{ErrorBadRequest, ErrorUnauthorized, ErrorUnprocessableEntity},
+    post, web, HttpRequest,
+};
+use discord::InteractionType;
 
 mod auth;
 mod discord;
 
+type Error = String;
+type Result<T> = std::result::Result<T, Error>;
+
+fn msg<S: ToString>(msg: S) -> Error {
+    msg.to_string()
+}
+
+fn err<T, S: ToString>(message: S) -> Result<T> {
+    Err(msg(message))
+}
+
+fn extract_header<'a>(req: &'a HttpRequest, header: &str) -> Result<&'a str> {
+    match req.headers().get(header) {
+        Some(value) => value.to_str().map_err(msg),
+        None => err(format!("missing {} header", header)),
+    }
+}
+
+fn e400<S: ToString>(message: S) -> actix_web::Error {
+    ErrorBadRequest(msg(message))
+}
+
+fn e422<S: ToString>(message: S) -> actix_web::Error {
+    ErrorUnprocessableEntity(msg(message))
+}
+
+fn e401<S: ToString>(message: S) -> actix_web::Error {
+    ErrorUnauthorized(msg(message))
+}
+
 #[post("/api/interactions")]
-async fn interactions(interaction: web::Json<discord::Interaction>) -> impl Responder {
-    dbg!(interaction);
-    web::Json(discord::InteractionResponse::pong())
+async fn interactions(
+    req: HttpRequest,
+    body: web::Bytes,
+) -> std::result::Result<web::Json<discord::InteractionResponse>, actix_web::Error> {
+    let body = String::from_utf8(body.to_vec()).map_err(e422)?;
+    let sighex = extract_header(&req, "X-Signature-Ed25519").map_err(e400)?;
+    let timestamp = extract_header(&req, "X-Signature-Timestamp").map_err(e400)?;
+    if !auth::validate(sighex, timestamp, &body).map_err(e400)? {
+        return Err(e401("invalid request signature"));
+    }
+
+    let interaction = serde_json::de::from_str::<discord::Interaction>(&body).map_err(e422)?;
+    dbg!(&interaction);
+
+    let resp = match interaction.inttype() {
+        InteractionType::Ping => discord::InteractionResponse::pong(),
+        _ => return Err(e422("unhandled interaction type")),
+    };
+
+    Ok(web::Json(resp))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init();
+
     actix_web::HttpServer::new(|| {
         actix_web::App::new()
-            .wrap_fn(|req, srv| {
-                req.headers().get("X-Signature-Ed25519");
-
-                let fut = srv.call(req);
-                async {
-                    let res = fut.await?;
-                    Ok(res)
-                }
-            })
+            .wrap(actix_web::middleware::Logger::default())
             .service(interactions)
     })
     .bind(("127.0.0.1", 8080))?
